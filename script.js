016ad7d0b1727ef16022ffa38dadeb9dde6f097f
Jay Steinle <jay.steinle62@gmail.com>
2020-11-19 21:02:04 -0600

    final

--- script.js
+++ script.js
@@ -1,1 +1,40 @@
+window.addEventListener("load", function() {
+    fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function(response){
+        return response.json().then(function(json){
+
+            let container = document.getElementById("container");
+
+            json.sort(function compare(a,b){
+                return(b.hoursInSpace- a.hoursInSpace);
+            })
+
+            let index = 0
+
+            for(let astronaut of json){
+                let activeLine = ""
+                // console.log(astronaut)
+                index++;
+                if(astronaut.active === true){
+                    activeLine = `<font color="green"> Active: ${astronaut.active}</font>`
+                }else{
+                    activeLine = `<font color="red"> Active: ${astronaut.active}</font>`
+                }
+            
+                container.innerHTML += `<div class="astronaut">
+                <div class = "bio">
+                    <h3>${astronaut.firstName} ${astronaut.lastName}</h3>
+                    <ul>
+                        <li>Hours in space: ${astronaut.hoursInSpace}</li>
+                        <li>${activeLine}</li>
+                        <li>Skills: ${astronaut.skills.join(", ")}</li>
+                    </ul>
+                </div>
+                <img class="avatar" src = ${astronaut.picture}>
+                `;
+            }
+
+            })
+        });
+    });
+// });
 // TODO: add code here
